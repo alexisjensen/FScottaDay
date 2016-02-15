d5ecdd435310921bc9ba1b0c5ba7abf60fbe5f81
Alexis Jensen <jensen.alexis@gmail.com>
2016-02-14 16:57:18 -0800

    Initial Commit

---
+++ FScottaDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FScottaDay
+//
+//  Created by Alexis Jensen on 2016-02-14.
+//  Copyright © 2016 beep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
